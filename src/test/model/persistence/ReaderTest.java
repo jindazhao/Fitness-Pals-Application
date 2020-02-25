be3172bd1df5eadc0a83de6961b9d7d96fff13f9
Jinda Zhao <jindazhao@hotmail.ca>
2020-02-24 21:05:39 -0800

    Implemented tests for both reader and writer, still fail though

--- src/test/model/persistence/ReaderTest.java
+++ src/test/model/persistence/ReaderTest.java
@@ -1,4 +1,36 @@
 package model.persistence;
 
+import model.FitnessPlan;
+import org.junit.jupiter.api.Test;
+import persistence.Reader;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ReaderTest {
+    @Test
+    void testParseFitnessFile1() {
+        try {
+            FitnessPlan fitnessPlan = Reader.readFitnessPlan(new File("./data/testFitnessPlanFile1.txt"));
+            assertEquals("My Fitness Plan", fitnessPlan.getFitnessPlanName());
+            assertEquals(1, fitnessPlan.getTotalNumberOfGoals());
+
+
+        } catch (IOException e) {
+            fail("IOEception should have been thrown");
+        }
+    }
+
+    @Test
+    void testIOException() {
+        try {
+            Reader.readFitnessPlan(new File("./path/does/not/exist/textFitnessPlan.txt"));
+        } catch (IOException e) {
+            // expected
+        }
+    }
+
 }
--- src/test/model/persistence/WriterTest.java
+++ src/test/model/persistence/WriterTest.java
@@ -1,4 +1,63 @@
 package model.persistence;
 
+import model.Exercise;
+import model.FitnessPlan;
+import model.Goal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import persistence.Reader;
+import persistence.Writer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class WriterTest {
+    private static final String TEST_FILE = "./data/testFitnessPlan.txt";
+    private Writer testWriter;
+    private FitnessPlan fitnessPlan;
+    private Goal goal;
+    private Exercise exercise;
+
+
+
+
+    @BeforeEach
+    void runBefore() throws FileNotFoundException, UnsupportedEncodingException {
+        testWriter = new Writer(new File(TEST_FILE));
+        fitnessPlan = new FitnessPlan();
+        goal = new Goal("Lose Weight", "00/00/0000", exercise);
+        exercise = new Exercise("Planks", 100, 100);
+
+    }
+
+
+    @Test
+    void testWriteFitnessPlan() {
+
+        fitnessPlan.addGoal(goal);
+        fitnessPlan.updateFitnessPlanName("My Fitness Plan");
+
+        // save fitness name and list of goals
+        testWriter.write(fitnessPlan);
+        testWriter.close();
+        // now read them back in and verify that the accounts have the expected values
+        try {
+            FitnessPlan fitnessPlan = Reader.readFitnessPlan(new File(TEST_FILE));
+            assertEquals("My Fitness Plan", fitnessPlan.getFitnessPlanName());
+            assertEquals(1, fitnessPlan.getTotalNumberOfGoals());
+
+
+        } catch (IOException e) {
+            fail("IOException should not have been thrown");
+        }
+
+
+    }
+
+
 }
